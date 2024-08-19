5d5de48a6b607d438dd7517e81e3a4be45830083
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-18 17:13:21 -0700

    Added a function to add a role

--- index.js
+++ index.js
@@ -98,38 +98,78 @@ function addDepartment() {
     });
 }
 //Function to add a role
-pool.query('SELECT * FROM department', (err, res) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  //Get the department choices from the database
-  const departments = res.map(({name, id})=>({name, id}));
-  inquirer
-    .prompt([
-      {
-        name: "title",
-        type: "input",
-        message: "What is the title of the role?"
-      },
-      {
-        name: "salary",
-        type: "input",
-        message: "What is the salary of the role?"
-      },
-      {
-        name: "department",
-        type: "list",
-        message: "What department does the role belong to?",
-        choices: [...departments.map(({ name, id }) => ({ name, value: id }), { name: "Create a New Department", value: null })]
-  
+function addRole() {
+  pool.query("SELECT name, id FROM department", (err, res) => {
+      if (err) {
+          console.log(err);
+          return;
       }
-    ]).then(data => {
-      pool.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [data.title, data.salary, data.department], (err, res) => {
-        if (err) throw err;
-        console.log(`Added ${data.title} to roles.`);
-        startPrompt();
+
+      // gets all the department names and ids from the response and puts it into an array called departments
+      const departments = res.map(({ name, id }) => ({ name, id }));
+     //Use inquirer prompt to ask questions
+      inquirer.prompt([
+        //What is the name of the role?
+          {
+              name: "title",
+              type: "input",
+              message: "What is the name of the role you would like to add?"
+          },
+           //What is the salary for the role?
+          {
+              name: "salary",
+              type: "input",
+              message: "What is the salary for the role?"
+          },
+        //What department would you like to add this role to?
+          {
+              name: "department",
+              type: "list",
+              message: "What department would you like to add this role to?",
+              choices: [...departments.map(({ name, id }) => ({ name, value: id }), { name: "Create a New Department", value: null })]
+          }
+      ]).then(answers => {
+          const { title, salary, department } = answers;
+        //If the department doesn't exist, ask the user to create a new department
+          if (department === null) {
+              inquirer.prompt([
+                  {
+                      name: "newDepartment",
+                      type: "input",
+                      message: "What is the name of the department you would like to create?"
+                  }
+              ]).then(newDepartmentAnswer => {
+                  pool.query("INSERT INTO department (name) VALUES ($1)", [newDepartmentAnswer.newDepartment], (err, res) => {
+                      if (err) {
+                          console.log(err);
+                      } else {
+                          const newDepartmentName = res.rows[0].id;
+                          pool.query("INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)", [title, salary, newDepartmentName], (err, res) => {
+                              if (err) {
+                                  console.log(err);
+                              } else {
+                                  console.log("Role added successfully");
+                                  //Return to the startPrompt function
+                                  startPrompt();
+                              }
+                          })
+                      }
+                  })
+              })
+          } else {
+            //If the department does exist, add the role to that department
+            //Add the role to the database
+              pool.query("INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)", [title, salary, department], (err, res) => {
+                  if (err) {
+                      console.log(err);
+                  } else {
+                      console.log("Role added successfully");
+                      //Return to the startPrompt function
+                      startPrompt();
+                  }
+              });
+          }
       });
-    });
-});
+  });
+}
 //Function to add an employee
